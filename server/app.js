3323b59d598cecf7f8ec1ef04219116b48180a80
Nikitha Kasala <nikithakasala98@gmail.com>
2022-10-18 22:05:13 +0530

    deploy netify 3

--- server/app.js
+++ server/app.js
@@ -111,12 +111,12 @@ app.post("/userData", async (req, res) => {
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
-}
-app.get('*',(req, res) => {
+    app.get('*',(req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
+}
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server Started");
 });
 
