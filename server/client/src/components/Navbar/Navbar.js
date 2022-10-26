65c0fe7b70333405b237ca9d657bd443a8ce79a1
Nikitha Kasala <nikithakasala98@gmail.com>
2022-10-27 02:18:54 +0530

    changing navbar

--- server/client/src/components/Navbar/Navbar.js
+++ server/client/src/components/Navbar/Navbar.js
@@ -33,6 +33,7 @@ class Navbar extends Component{
                 <ul className='menu'>
                     <li><Link to='/'>Home</Link></li> 
                     <li><Link to='/features'>Features</Link></li>
+                    <li><Link to='/sign-up'>Login</Link></li>
                     <li><Link to='/about'>About</Link></li>
                     <li><Link to='/contact'>Contact US</Link></li>
                     <li><Link to='/sign-in' className="hello" onClick={this.clearStorage} >Logout</Link></li>
